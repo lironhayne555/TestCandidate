476fb256fab173b6acc99e1af631de158c8cc4ab
liron hayne <lironhayne555@gmail.com>
2023-03-17 14:27:29 +0200

    first commit

---
+++ 4. TestCandidate.Data/TestCandidate.Data.Components/CampaignActivity.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestCandidate.Data.Components
+{
+    class CampaignActivity
+    {
+        public int Code { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+       public Campaign Campaign { get; set; }
+    }
+}
